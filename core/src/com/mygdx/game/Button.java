6667406569cb33e4541f0e0b21328acf1b5c05b5
Kostya11233 <kostyatimofeev003@gmail.com>
2026-05-12 19:14:06 +0300

    Update Button.java

--- core/src/com/mygdx/game/Button.java
+++ core/src/com/mygdx/game/Button.java
@@ -1,4 +1,5 @@
 package com.mygdx.game;
+//https://www.dropbox.com/scl/fi/94o3hfv6qpxaucdk4jhzc/IMG_20260512_191321_204.jpg?rlkey=oi8bdfhkg53pqxdevi1523dwy&st=ey8166le&dl=0
 //https://www.dropbox.com/scl/fi/8tlazz9gusz834ddwvxqn/IMG_20260512_184238_812.jpg?rlkey=zosjgj98f81sspyx7lw2j03nd&st=ywvbazhb&dl=0
 //https://www.dropbox.com/scl/fi/b06rpq17uau7nzv5gbr81/IMG_20260512_181524.jpg?rlkey=od2p362m01m6uvyg42sh1fent&st=9dtiliwh&dl=0
 import com.badlogic.gdx.graphics.Color;
